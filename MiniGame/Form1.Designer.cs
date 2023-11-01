9266756796c9770c5416de21eaec6b7077e6182d
Eka <21106050023@student.uin-suka.ac.id>
2023-11-01 09:38:42 +0700

    Add project files.

---
+++ MiniGame/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MiniGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_sudoku = new System.Windows.Forms.Button();
+            this.bt_matching = new System.Windows.Forms.Button();
+            this.bt_flappy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_sudoku
+            // 
+            this.bt_sudoku.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_sudoku.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_sudoku.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bt_sudoku.FlatAppearance.BorderSize = 2;
+            this.bt_sudoku.Font = new System.Drawing.Font("Batang", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_sudoku.Location = new System.Drawing.Point(169, 207);
+            this.bt_sudoku.Name = "bt_sudoku";
+            this.bt_sudoku.Padding = new System.Windows.Forms.Padding(5);
+            this.bt_sudoku.Size = new System.Drawing.Size(195, 45);
+            this.bt_sudoku.TabIndex = 0;
+            this.bt_sudoku.Text = "Sudoku";
+            this.bt_sudoku.UseVisualStyleBackColor = false;
+            this.bt_sudoku.Click += new System.EventHandler(this.bt_sudoku_Click);
+            // 
+            // bt_matching
+            // 
+            this.bt_matching.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_matching.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_matching.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bt_matching.FlatAppearance.BorderSize = 2;
+            this.bt_matching.Font = new System.Drawing.Font("Batang", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_matching.Location = new System.Drawing.Point(169, 268);
+            this.bt_matching.Name = "bt_matching";
+            this.bt_matching.Padding = new System.Windows.Forms.Padding(5);
+            this.bt_matching.Size = new System.Drawing.Size(195, 45);
+            this.bt_matching.TabIndex = 1;
+            this.bt_matching.Text = "Matching Game";
+            this.bt_matching.UseVisualStyleBackColor = false;
+            // 
+            // bt_flappy
+            // 
+            this.bt_flappy.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_flappy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_flappy.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bt_flappy.FlatAppearance.BorderSize = 2;
+            this.bt_flappy.Font = new System.Drawing.Font("Batang", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_flappy.Location = new System.Drawing.Point(169, 332);
+            this.bt_flappy.Name = "bt_flappy";
+            this.bt_flappy.Padding = new System.Windows.Forms.Padding(5);
+            this.bt_flappy.Size = new System.Drawing.Size(195, 45);
+            this.bt_flappy.TabIndex = 2;
+            this.bt_flappy.Text = "Flappy Bird";
+            this.bt_flappy.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Orbitron", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 105);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mini Games";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_flappy);
+            this.Controls.Add(this.bt_matching);
+            this.Controls.Add(this.bt_sudoku);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Games";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_sudoku;
+        private System.Windows.Forms.Button bt_matching;
+        private System.Windows.Forms.Button bt_flappy;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MiniGame/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_sudoku_Click(object sender, EventArgs e)
+        {
+            // Sembunyikan form utama
+            this.Hide();
+
+            // Membuat instance form baru
+            Form2 form2 = new Form2(); // Ganti "Form2" dengan nama form yang Anda buat
+
+            // Menampilkan form baru
+            form2.Show();
+        }
+    }
+}

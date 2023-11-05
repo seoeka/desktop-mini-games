ae1ea1d40af5e72eae169a2d5312e1ee7a42b59d
Eka <21106050023@student.uin-suka.ac.id>
2023-11-05 08:02:33 +0700

    add resources for flappy bird and done flappy bird features

--- MiniGame/Form4.Designer.cs
+++ MiniGame/Form4.Designer.cs
@@ -28,71 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.trunkTop = new System.Windows.Forms.PictureBox();
+            this.trunkBottom = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.timerFlappy = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
             this.bt_home = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
+            this.bt_start = new System.Windows.Forms.Button();
+            this.bt_restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trunkTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trunkBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
             this.SuspendLayout();
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = global::MiniGame.Properties.Resources.pink_bird;
+            this.flappyBird.Location = new System.Drawing.Point(111, 140);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(56, 40);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 23;
+            this.flappyBird.TabStop = false;
+            // 
+            // trunkTop
+            // 
+            this.trunkTop.Image = global::MiniGame.Properties.Resources.trunkdown;
+            this.trunkTop.Location = new System.Drawing.Point(380, -188);
+            this.trunkTop.Name = "trunkTop";
+            this.trunkTop.Size = new System.Drawing.Size(84, 334);
+            this.trunkTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trunkTop.TabIndex = 24;
+            this.trunkTop.TabStop = false;
+            // 
+            // trunkBottom
+            // 
+            this.trunkBottom.Image = global::MiniGame.Properties.Resources.trunk;
+            this.trunkBottom.Location = new System.Drawing.Point(309, 257);
+            this.trunkBottom.Name = "trunkBottom";
+            this.trunkBottom.Size = new System.Drawing.Size(84, 307);
+            this.trunkBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trunkBottom.TabIndex = 25;
+            this.trunkBottom.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.ground.Image = global::MiniGame.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-12, 386);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(562, 90);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 26;
+            this.ground.TabStop = false;
+            // 
+            // timerFlappy
+            // 
+            this.timerFlappy.Interval = 30;
+            this.timerFlappy.Tick += new System.EventHandler(this.timerFlappyEvent);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.scoreLabel.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(27, 410);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(90, 25);
+            this.scoreLabel.TabIndex = 27;
+            this.scoreLabel.Text = "Score: 0";
             // 
             // bt_home
             // 
             this.bt_home.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_home.Location = new System.Drawing.Point(20, 388);
+            this.bt_home.Location = new System.Drawing.Point(27, 323);
             this.bt_home.Name = "bt_home";
-            this.bt_home.Size = new System.Drawing.Size(480, 34);
-            this.bt_home.TabIndex = 17;
+            this.bt_home.Size = new System.Drawing.Size(116, 34);
+            this.bt_home.TabIndex = 29;
+            this.bt_home.TabStop = false;
             this.bt_home.Text = "Homepage";
             this.bt_home.UseVisualStyleBackColor = true;
             this.bt_home.Click += new System.EventHandler(this.bt_home_Click);
             // 
-            // button2
+            // bt_start
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(390, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 34);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "Restart";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bt_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_start.Location = new System.Drawing.Point(27, 283);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(116, 34);
+            this.bt_start.TabIndex = 30;
+            this.bt_start.TabStop = false;
+            this.bt_start.Text = "Start";
+            this.bt_start.UseVisualStyleBackColor = true;
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
             // 
-            // panel1
+            // bt_restart
             // 
-            this.panel1.Location = new System.Drawing.Point(20, 19);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(350, 350);
-            this.panel1.TabIndex = 9;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(390, 59);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(110, 34);
-            this.button4.TabIndex = 18;
-            this.button4.Text = "Save Game";
-            this.button4.UseVisualStyleBackColor = true;
+            this.bt_restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_restart.Location = new System.Drawing.Point(27, 283);
+            this.bt_restart.Name = "bt_restart";
+            this.bt_restart.Size = new System.Drawing.Size(116, 34);
+            this.bt_restart.TabIndex = 31;
+            this.bt_restart.TabStop = false;
+            this.bt_restart.Text = "Restart";
+            this.bt_restart.UseVisualStyleBackColor = true;
+            this.bt_restart.Click += new System.EventHandler(this.bt_restart_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(534, 441);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.bt_restart);
+            this.Controls.Add(this.bt_start);
             this.Controls.Add(this.bt_home);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.trunkBottom);
+            this.Controls.Add(this.trunkTop);
+            this.Controls.Add(this.flappyBird);
             this.Name = "Form4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.flappyKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.flappyKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trunkTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trunkBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox trunkTop;
+        private System.Windows.Forms.PictureBox trunkBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Timer timerFlappy;
+        private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.Button bt_home;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button bt_start;
+        private System.Windows.Forms.Button bt_restart;
     }
 }
--- MiniGame/Form4.cs
+++ MiniGame/Form4.cs
@@ -7,23 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace MiniGame
 {
     public partial class Form4 : Form
     {
+        int trunkSpeed = 6; // default trunk speed defined with an integer
+        int gravity = 5; // default gravity speed defined with an integer
+        int score = 0; // default score integer set to 0
         public Form4()
         {
             InitializeComponent();
+            bt_restart.Visible = false;
+            bt_restart.Enabled = false;
+        }
+
+        private void flappyKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 8;
+            }
+        }
+
+        private void flappyKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void timerFlappyEvent(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravity;
+            trunkTop.Left -= trunkSpeed; // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
+            trunkBottom.Left -= trunkSpeed; // link the bottom pipes left position to the pipe speed integer, it will reduce the pipe speed value from the left position of the pipe picture box so it will move left with each tick
+            scoreLabel.Text = "Score: " + score; // show the current score on the score text label
+
+            if (trunkBottom.Left < -150)
+            {
+                trunkBottom.Left = 800;
+                score++;
+            }
+            if (trunkTop.Left < -180)
+            {
+                // if the top pipe location is -180 then we will reset the pipe back to the 950 and add 1 to the score
+                trunkTop.Left = 950;
+                score++;
+            }
+
+            if (score > 5 && score <= 10)
+            {
+                trunkSpeed = 8;
+            }
+
+            if (score > 10 && score <= 15)
+            {
+                trunkSpeed = 10;
+            }
+
+            if (flappyBird.Bounds.IntersectsWith(trunkBottom.Bounds) ||
+                flappyBird.Bounds.IntersectsWith(trunkTop.Bounds) ||
+                flappyBird.Bounds.IntersectsWith(ground.Bounds) || flappyBird.Top < -25
+                )
+            {
+                // if any of the conditions are met from above then we will run the end game function
+                endGame();
+            }
+
+            
+        }
+
+        private void endGame()
+        {
+            timerFlappy.Stop(); // stop the main timer
+            scoreLabel.Text = "Game over!!!";
+            bt_home.Visible = true;
+            bt_home.Enabled = true;
+            bt_restart.Visible = true;
+            bt_restart.Enabled = true;
+        }
+
+        private void bt_start_Click(object sender, EventArgs e)
+        {
+            bt_start.Visible = false;
+            bt_start.Enabled = false;
+            bt_home.Visible = false;
+            bt_home.Enabled = false;
+            timerFlappy.Start(); // stop the main timer
+
         }
 
         private void bt_home_Click(object sender, EventArgs e)
         {
-            {
-                this.Hide();
-                Form1 form1 = new Form1();
-                form1.Show();
-            }
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        private void bt_restart_Click(object sender, EventArgs e)
+        {
+            trunkSpeed = 6;
+            gravity = 5;
+            score = 0;
+            scoreLabel.Text = "Score: " + score;
+            flappyBird.Location = new Point(53, 175);
+            trunkTop.Location = new Point(380, -188);
+            trunkBottom.Location = new Point(309, 257);
+            bt_restart.Visible = false;
+            bt_restart.Enabled = false;
+            bt_home.Visible = false;
+            bt_home.Enabled = false;
+            timerFlappy.Start();
         }
     }
 }
